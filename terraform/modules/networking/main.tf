297a7af378204dce1d4530ddff162acc994adc0e
modelav <mo.delav01@gmail.com>
2026-08-22 21:58:04 +0330

    feat: open port 80 and deploy nginx container on app server

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -64,6 +64,15 @@ resource "aws_security_group" "main" {
     cidr_blocks = [var.allowed_ssh_cidr]
   }
 
+  # HTTP inbound for the application
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   # Outbound access - required for apt updates, Docker pulls, etc.
   egress {
