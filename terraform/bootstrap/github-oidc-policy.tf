abd38670b6ba4eb43b32f247ec5932a9f52e045f
modelav <mo.delav01@gmail.com>
2026-09-07 18:46:11 +0330

    fix: add github-actions required permissions

--- terraform/bootstrap/github-oidc-policy.tf
+++ terraform/bootstrap/github-oidc-policy.tf
@@ -19,18 +19,12 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
         "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:environment:production"
       ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:ref"
-      values   = ["refs/heads/main"]
     }
   }
 }
