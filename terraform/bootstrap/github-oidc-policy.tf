d25d5ecdfa843c2db6b00c316433a7a60bd06b1b
modelav <mo.delav01@gmail.com>
2026-09-07 17:38:06 +0330

    fix: align github oidc trust with production environment

--- terraform/bootstrap/github-oidc-policy.tf
+++ terraform/bootstrap/github-oidc-policy.tf
@@ -23,8 +23,14 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/main"
+        "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:environment:production"
       ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:ref"
+      values   = ["refs/heads/main"]
     }
   }
 }
